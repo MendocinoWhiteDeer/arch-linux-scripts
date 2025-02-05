966d41318ddd1e09840affb384f08527784913b8
Taylor Wampler <taylorjwampler@proton.me>
2025-02-05 17:43:37 -0600

    chicken-tcp.sh: Simplified code to fix bug
    Original code had lag and would always crash eventually. I believe it is
    tied to the parts of the script which use netcat's "-z" flag.

--- chicken-tcp.sh
+++ chicken-tcp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# A script for Chicken over tcp; tested on Arch Linux
+# A script for Chicken over TCP; tested on Arch Linux
 # If server is already running on localhost at the specified port, it will just connect
 # Dependencies: 
 # 	System packages: netcat, chicken
@@ -7,26 +7,12 @@
 
 CHICKEN_PORT=1234
 MAX_ATTEMPTS=10
-SLEEP_TIME=0.1
+SLEEP_TIME=0.5
 
-# Check if server is already running at specified port
-if nc -z localhost $CHICKEN_PORT; then
-	echo "Server is already running. Connecting to whatever is there ..."
-	exec nc localhost $CHICKEN_PORT
-fi
-
-# Start chicken-csi with nrepl in the background
 echo "Starting server ..."
-(chicken-csi -R nrepl -P "(nrepl $CHICKEN_PORT)" &) 
-
-# Make multiple attempts to connect
-echo "Attempting to connect a maximum of $MAX_ATTEMPTS times every $SLEEP_TIME seconds."
-for ((i=l; i<=MAX_ATTEMPTS; i++)); do
-	if nc -z localhost $CHICKEN_PORT; then
-		exec nc localhost $CHICKEN_PORT
-	fi
-	sleep $SLEEP_TIME
-done
+chicken-csi -R nrepl -P "(nrepl $CHICKEN_PORT)" & 
+sleep $SLEEP_TIME
+nc localhost $CHICKEN_PORT
+exit 0
 
 echo "Failed to connect to localhost at port $CHICKEN_PORT after multiple attempts." >&2
-exit 1
